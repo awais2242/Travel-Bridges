3c3aef4919d18998f06d45b8d7ba04c060eef66c
awaisasif <awaissasif4@gmail.com>
2023-08-08 17:41:52 +0500

    header home issue fixed

--- src/lib/private-route.tsx
+++ src/lib/private-route.tsx
@@ -12,12 +12,12 @@ const PrivateRoute: React.FC = ({ children }) => {
   if (!isUser && !isAuthorized) {
     return (
       <div className="relative flex justify-center w-full min-h-screen py-5 md:py-8">
-        <button
+        {/* <button
           className="absolute flex items-center justify-center w-8 h-8 text-gray-200 transition-colors md:w-16 md:h-16 top-5 md:top-1/2 ltr:left-5 rtl:right-5 ltr:md:left-10 rtl:md:right-10 md:-mt-8 md:text-gray-300 hover:text-gray-400"
           onClick={router.back}
         >
           <BackArrowRound />
-        </button>
+        </button> */}
         <div className="flex flex-col my-auto">
           <LoginView />
         </div>
